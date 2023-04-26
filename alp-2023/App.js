5c25fda093e858f72ba7f28f0db5b7c41e4ff1cb
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2023-04-25 21:56:40 -0300

    react navigation

--- alp-2023/App.js
+++ alp-2023/App.js
@@ -2,17 +2,38 @@ import React, {useState} from 'react';
 import { 
   StyleSheet, 
   View, 
-  Text
+  Text,
+  Pressable
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-function screenA() {
+function ScreenA({navigation}) {
+  const onPressHandler = () => {
+    navigation.navigate("ScreenB")
+  }
+
   return(
-    <View>
-      <Text>Screen A</Text>
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen A</Text>
+      <Pressable 
+        style={styles.button}
+        onPress={onPressHandler}>
+        <Text style={[styles.text,{
+          color:"#FFF",
+          fontSize: 20,
+        }]}>Go to screen B</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+function ScreenB() {
+  return(
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen B</Text>
     </View>
   )
 }
@@ -21,13 +42,40 @@ function App() {
   return(
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="screenA"
-          component={screenA}
+        <Stack.Screen 
+          name="ScreenA"
+          component={ScreenA}
+          options={{
+            header: ()=>{null}
+          }}
+        />
+        <Stack.Screen 
+          name="ScreenB"
+          component={ScreenB}
         />
       </Stack.Navigator>
     </NavigationContainer>
   )
 }
 
-export default App() 
+const styles = StyleSheet.create({
+  body: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  text: {
+    fontSize:40,
+    fontWeight: 'bold'
+  },
+  button: {
+    backgroundColor:'#09f',
+    width: 240,
+    height: 70,
+    justifyContent:'center',
+    alignItems:'center',
+    margin: 10,
+  }
+})
+
+export default App
