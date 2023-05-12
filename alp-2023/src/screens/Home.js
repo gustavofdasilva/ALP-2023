34e3340f3f796055b8b78ba33f6319bf056fd564
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2023-05-11 21:59:57 -0300

    async storage

--- alp-2023/src/screens/Home.js
+++ alp-2023/src/screens/Home.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { 
     StyleSheet, 
     View, 
     Text,
-    Pressable
+    Pressable,
+    Alert
   } from 'react-native';
 import globalStyle from '../utils/globalStyle';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
   export default function Home({navigation}) {
+
+    const [userName,setUserName] = useState('')
+
+    useEffect(() => {
+        getData();
+    }, [])
+    
+
+    const getData = async () => {
+      try {
+        await AsyncStorage.getItem('userName')
+          .then(value => {
+            if (value!=null) {
+              setUserName(value)
+            }
+          })
+      } catch (error) {
+        Alert.alert("Warning",error)
+      }
+    }
   
     return(
       <View style={globalStyle.body}>
         <Text style={globalStyle.text}>Home</Text>
+        <Text style={globalStyle.text}>Welcome {userName}</Text>
         <Pressable 
           style={globalStyle.button}
           onPress={()=>navigation.push('Login')}>
--- alp-2023/src/screens/login.js
+++ alp-2023/src/screens/login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
     StyleSheet, 
     View, 
@@ -9,12 +9,44 @@ import {
 import globalStyle from '../utils/globalStyle';
 import { TextInput } from 'react-native-gesture-handler';
 import LoginButton from '../utils/button';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
   export default function Login({navigation}) {
     
     const [userName, setUserName] = useState('');
-    const setData = ()=>{
-        Alert.alert('Hello')
+
+      useEffect(() => {
+        getData();
+    }, [])
+
+    
+    
+
+    const getData = async () => {
+      try {
+        await AsyncStorage.getItem('userName')
+          .then(value => {
+            if (value!=null) {
+              navigation.navigate('Home');
+            }
+          })
+      } catch (error) {
+        Alert.alert("Warning",error)
+      }
+    }
+
+    const setData = async () => {
+        if(userName.length==0) {
+          Alert.alert("Warning!","Please write a username")
+        } else {
+          try {
+            await AsyncStorage.setItem('userName',userName)
+              
+          } catch (e) {
+            Alert.alert("Warning!",e);
+          }
+          navigation.navigate('Home');
+        }
     }
 
 
